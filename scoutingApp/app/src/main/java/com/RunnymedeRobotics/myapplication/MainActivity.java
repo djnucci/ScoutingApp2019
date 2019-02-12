304770a72326a18718c4890292ef9674513e88c9
djnucci <djnucci@hotmail.com>
2019-02-12 14:44:33 -0500

    lil bit of work on the navigation

--- scoutingApp/app/src/main/java/com/RunnymedeRobotics/myapplication/MainActivity.java
+++ scoutingApp/app/src/main/java/com/RunnymedeRobotics/myapplication/MainActivity.java
@@ -1,9 +1,7 @@
 package com.RunnymedeRobotics.myapplication;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.v4.app.Fragment;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -23,23 +21,18 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new basicFragment(R.layout.app_bar_main)).commit();
+            navigationView.setCheckedItem(R.id.nav_match_schedule);
+        }
     }
 
     @Override
@@ -80,17 +73,14 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        Fragment f;
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        switch (id){
+            case R.id.nav_match_schedule:
+                f = new basicFragment(R.layout.scouting_start_page);
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,f).commit();
+                break;
+            case R.id.nav_gallery:
 
         }
 
---
+++ scoutingApp/app/src/main/java/com/RunnymedeRobotics/myapplication/basicFragment.java
@@ -0,0 +1,26 @@
+package com.RunnymedeRobotics.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class basicFragment extends Fragment {
+
+    private int layout;
+
+    public basicFragment(){}
+
+    public basicFragment(int layout){
+        this.layout = layout;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(this.layout, container, false);
+    }
+}
