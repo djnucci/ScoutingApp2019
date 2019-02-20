f9a833958dc3215405d8f1d3260ef2c8a834a858
Simar <simarpal.kalsi@gmail.com>
2019-02-19 21:45:10 -0500

    Class restructure and CALL API complete
    Accidently added the pojo and jsonqueue packages in test folder instead of the main so moved it to the right place. made the CallAPI class that gets the queuewrapper class passed to it and then post the serizazed version of the class into the glassfish server. Please add dependencies to build gradle.
    
        compile group: 'com.google.code.gson', name: 'gson', version: '2.7'
        compile 'org.apache.httpcomponents:httpclient:4.5'
        compile group: 'org.apache.commons', name: 'commons-io', version: '1.3.2'
    
    this throws an erorr in the build gradle but the errors in the code regarding using the proper library disappear. need to furthur investigate which dependencies to use.

---
+++ scoutingApp/app/src/main/java/com/RunnymedeRobotics/myapplication/apicalls/CallAPI.java
@@ -0,0 +1,65 @@
+package com.example.simar.androidsqlitetest.apicalls;
+
+import com.example.simar.androidsqlitetest.jsonqueue.QueueWrapper;
+import com.example.simar.androidsqlitetest.pojo.SubmitMatch;
+import com.google.gson.Gson;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Simar on 2/18/2019.
+ */
+
+public class CallAPI {
+
+    public static String serverIP = "";
+
+    /**
+     * Post local queue wrapper to server
+     * @param queueWrapper
+     */
+    public static String submitLocalQueue(QueueWrapper queueWrapper){
+        if(serverIP.equals("")){
+            System.out.println("Please set value to serverIP");
+            return "Please set value to serverIP";
+        }
+
+        //specifies http post to server ip and creates httppost object
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(serverIP);
+
+
+        try {
+            //Creates name value pair to pass onto the server
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+        //The queue name value pair coincides on the java jax-rs server side
+        nameValuePairs.add(new BasicNameValuePair("queue",(new Gson()).toJson(queueWrapper)));
+        //Encodes the request to urlencodeded to conciede with the server side
+        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        //Executes http response
+        HttpResponse httpResponse = httpclient.execute(httppost);
+        //returns the response from server if any
+        return "Post Completed ==> " + httpResponse.toString();
+    }
+    catch (ClientProtocolException e){
+        e.printStackTrace();
+        return "Post Error";
+    }
+    catch (IOException  e){
+        e.printStackTrace();
+        return "Post Error";
+    }
+}
+
+}
