5f177c841be76afee0c8904f76099488f369f177
Harkirat Khurana <mynameis4472@users.noreply.github.com>
2019-02-28 13:57:08 -0500

    Need to change Setup screens levels from buttons to radiobuttons. Created setup screen class.

---
+++ scoutingApp/app/src/main/java/com/RunnymedeRobotics/myapplication/fragment/SetupFragment.java
@@ -0,0 +1,92 @@
+package com.RunnymedeRobotics.myapplication.fragment;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+
+import com.RunnymedeRobotics.myapplication.MainActivity;
+import com.RunnymedeRobotics.myapplication.R;
+import com.RunnymedeRobotics.myapplication.datastructureclasses.Auto;
+
+import org.apache.http.impl.cookie.BestMatchSpec;
+
+public class SetupFragment extends BasicFragment {
+    private int layout;
+
+    RadioButton levelOneBtn;
+    RadioButton levelTwoRightBtn;
+    RadioButton levelTwoLeftBtn;
+
+    RadioButton hatchPreload;
+    RadioButton cargoPreload;
+    RadioButton noPrelaod;
+
+    Button matchPlayTab;
+    Button endGameTab;
+
+    public SetupFragment(){}
+
+    @SuppressLint("ValidFragment")
+    public SetupFragment(int layout){
+        this.layout = layout;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(this.layout, container, false);
+
+
+        levelOneBtn = (RadioButton) view.findViewById(R.id.setup_level_one);
+        levelTwoLeftBtn = (RadioButton) view.findViewById(R.id.setup_level_two_left);
+        levelTwoRightBtn = (RadioButton) view.findViewById(R.id.setup_level_two_left);
+
+        hatchPreload = (RadioButton) view.findViewById(R.id.setup_hatch_radioButton);
+        cargoPreload = (RadioButton) view.findViewById((R.id.setup_cargo_radioButton));
+        noPrelaod = (RadioButton)  view.findViewById(R.id.setup_nothing_radioButton);
+
+        matchPlayTab = (Button) view.findViewById(R.id.scouting_setup_match);
+        endGameTab = (Button) view.findViewById(R.id.scouting_setup_endgame);
+
+        matchPlayTab.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                Auto auto = new Auto();
+                                                if(cargoPreload.isChecked()){
+                                                    auto.setStartingObj('C');
+                                                    auto.setAutoPreload('1');
+                                                }
+                                                else if (hatchPreload.isChecked()){
+                                                    auto.setStartingObj('H');
+                                                    auto.setAutoPreload('1');
+                                                }
+                                                else if(noPrelaod.isChecked()){
+                                                    auto.setStartingObj('N');
+                                                    auto.setAutoPreload('1');
+                                                }
+
+                                                if(levelOneBtn.isChecked()){
+                                                    auto.setAutoLvl(1);
+                                                }
+                                                if (levelTwoLeftBtn.isChecked() || levelTwoRightBtn.isChecked()){
+                                                    auto.setAutoLvl(2);
+                                                }
+                                                MainActivity.globalSubmitMatch.setAuto(auto);
+
+                                            }
+                                        });
+
+
+
+
+
+        return view;
+    }
+}
